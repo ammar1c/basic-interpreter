d3cf8fc925265e39551919481e197f8ae56fd5e9
Ammar Al-Batool <ammara@spotify.com>
2023-02-01 19:25:28 +0100

    interpreter

--- basic.py
+++ basic.py
@@ -35,6 +35,29 @@ class InvalidSyntaxError(Error):
     def __init__(self, pos_start, pos_end, details):
         super().__init__(pos_start, pos_end, "Invalid Syntax", details)
 
+class RTError(Error):
+    def __init__(self, pos_start, pos_end, details, context):
+        super().__init__(pos_start, pos_end, "Runtime Error", details)
+        self.context = context
+    def as_string(self):
+        result = self.generate_traceback()
+        result += f"{self.error_name}: {self.details}"
+        result += "\n\n" + string_with_arrows(self.pos_start.file_text, self.pos_start, self.pos_end)
+        return result
+
+    def generate_traceback(self):
+        result = ''
+        pos = self.pos_start
+        ctx = self.context
+        while ctx:
+            result = f' File {pos.file_name}, line {str(pos.line + 1)}, in {ctx.display_name} \n' + result
+            pos = ctx.parent_entry_pos
+            ctx = ctx.parent
+        return "Traceback (most recent call last): \n" + result
+
+
+
+
 class Position:
     def __init__(self, index, line, column, file_name, file_text):
         self.index = index
@@ -138,7 +161,8 @@ class Lexer:
 class NumberNode:
     def __init__(self, token):
         self.token = token
-
+        self.pos_start = self.token.pos_start
+        self.pos_end = self.token.pos_end
     def __repr__(self):
         return f"{self.token}"
 
@@ -148,6 +172,8 @@ class BinOpNode:
         self.left_node = left_node
         self.op_token = op_token
         self.right_node = right_node
+        self.pos_start = self.left_node.pos_start
+        self.pos_end = self.right_node.pos_end
 
     def __repr__(self):
         return f"({self.left_node}, {self.op_token}, {self.right_node})"
@@ -156,7 +182,8 @@ class UnaryOpNode:
     def __init__(self, op_token, node):
         self.op_token = op_token
         self.node = node
-
+        self.pos_start = self.op_token.pos_start
+        self.pos_end = node.pos_end
     def __repr__(self):
         return f"({self.op_token}, {self.node})"
 
@@ -216,8 +243,6 @@ class Parser:
                 ))
         return res.failure(InvalidSyntaxError(toke.pos_start, toke.pos_end, "Expected int or float"))
 
-
-
     def term(self):
         return self.bin_op(self.factor, (TokenType.MULTIPLY, TokenType.DIVIDE))
 
@@ -242,7 +267,99 @@ class Parser:
             return res.failure(InvalidSyntaxError(self.current_token.pos_start, self.current_token.pos_end, "Expected +-*/"))
         return res
 
+class RTResult:
+    def __init__(self):
+        self.value = None
+        self.error = None
+    def register(self, res):
+        if res.error:
+            self.error = res.error
+        return res.value
+    def success(self, value):
+        self.value = value
+        return self
+    def failure(self, error):
+        self.error = error
+        return self
+class Number:
+    def __init__(self, value):
+        self.value = value
+        self.set_pos()
+        self.set_context(None)
+    def set_context(self, context):
+        self.context = context
+        return self
+    def set_pos(self, pos_start = None, pos_end = None):
+        self.pos_start = pos_start
+        self.pos_end = pos_end
+        return self
+    def added_to(self, other):
+        if isinstance(other, Number):
+            return Number(self.value + other.value).set_context(self.context).set_pos(self.pos_start, other.pos_end), None
+    def subbed_by(self, other):
+        if isinstance(other, Number):
+            return Number(self.value - other.value).set_context(self.context).set_pos(self.pos_start, other.pos_end), None
+    def multed_by(self, other):
+        if isinstance(other, Number):
+            return Number(self.value * other.value).set_context(self.context).set_pos(self.pos_start, other.pos_end), None
+    def dived_by(self, other):
+        if isinstance(other, Number):
+            if other.value == 0:
+                return None, RTError(other.pos_start, other.pos_end, 'Division by zero', self.context)
+            return Number(self.value / other.value).set_context(self.context).set_pos(self.pos_start, other.pos_end), None
+    def __repr__(self):
+        return str(self.value)
 
+class Context:
+    def __init__(self, display_name, parent = None, parent_entry_pos = None):
+        self.display_name = display_name
+        self.parent = parent
+        self.parent_entry_pos = parent_entry_pos
+
+class Interpreter:
+
+    def interpret(self, node, context):
+        method_name = f'visit_{type(node).__name__}'
+        method = getattr(self, method_name, self.no_visit_method)
+        return method(node, context)
+
+    def no_visit_method(self, node, context):
+        raise Exception(f'No visit_{type(node).__name__} method defined')
+
+    def visit_NumberNode(self, node, context):
+
+        return RTResult().success(Number(node.token.value).set_context(context).set_pos(node.token.pos_start, node.token.pos_end))
+
+
+    def visit_BinOpNode(self, node, context):
+        res = RTResult()
+        left = res.register(self.interpret(node.left_node, context))
+        if res.error: return res
+        right = res.register(self.interpret(node.right_node, context))
+        if res.error: return res
+        if node.op_token.type == TokenType.PLUS:
+            result, error = left.added_to(right)
+        elif node.op_token.type == TokenType.MINUS:
+            result, error = left.subbed_by(right)
+        elif node.op_token.type == TokenType.MULTIPLY:
+            result, error = left.multed_by(right)
+        elif node.op_token.type == TokenType.DIVIDE:
+            result, error = left.dived_by(right)
+        if error:
+            return res.failure(error)
+        return res.success(result.set_pos(node.pos_start, node.pos_end))
+
+    def visit_UnaryOpNode(self, node, context):
+        res = RTResult()
+        number = res.register(self.interpret(node.node, context))
+        if res.error: return res
+        error = None
+        if node.op_token.type == TokenType.MINUS:
+            number, error = number.multed_by(Number(-1))
+        if error:
+            res.failure(error)
+        else:
+            res.success(number.set_pos(node.pos_start, node.pos_end))
 
 def run(filename, text):
     lexer = Lexer(filename, text)
@@ -252,5 +369,10 @@ def run(filename, text):
     parser = Parser(tokens)
 
     ast = parser.parse()
+    if ast.error:
+        return None, ast.error
 
-    return ast.node, ast.error
+    interpreter = Interpreter()
+    context = Context('<program>')
+    result = interpreter.interpret(ast.node, context)
+    return result.value, result.error
